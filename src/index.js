b6577227ea14228743b6284de6c4d525cfae18d6
Силин Дмитрий Сергеевич <dssilin@mts.ru>
2020-11-04 17:18:59 +0300

    work with styles and files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import Post from "./Post.js";
+// import json from "./assets/json.json";
+// import xml from "./assets/data.xml";
+// import csv from "./assets/data.csv";
+import WebpackImage from "./assets/webpack-logo.png";
+import "./styles/style.css";
 
-const post = new Post("Post title 1");
+console.log('image', WebpackImage);
+const post = new Post("Post title 1", WebpackImage);
 
 console.log("Post to string", post.toString());
+// console.log("JSON", json);
+// console.log("XML", xml);
+// console.log("CSV", csv);
