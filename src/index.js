a5d9f6a26b589b70e464800938b5740a6160358b
Силин Дмитрий Сергеевич <dssilin@mts.ru>
2020-11-04 17:45:03 +0300

    relosve + optimisation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import Post from "./Post.js";
+import * as $ from "jquery";
+import Post from "@models/Post.js";
 // import json from "./assets/json.json";
 // import xml from "./assets/data.xml";
 // import csv from "./assets/data.csv";
-import WebpackImage from "./assets/webpack-logo.png";
+import WebpackImage from "@/assets/webpack-logo.png";
 import "./styles/style.css";
 
-console.log('image', WebpackImage);
 const post = new Post("Post title 1", WebpackImage);
+$("pre").html(post.toString());
 
-console.log("Post to string", post.toString());
 // console.log("JSON", json);
 // console.log("XML", xml);
 // console.log("CSV", csv);
+// console.log("image", WebpackImage);
